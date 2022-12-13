375ffeb553d78d86bd0f29d237f6d7761b3595d1
wadudB <108853749+wadudB@users.noreply.github.com>
2022-12-13 04:27:32 +0000

    Create sqlScript.sh

---
+++ sqlScript.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+startDateTime="2023-01-01 06:29:26"
+endDateTime="2023-03-01 06:29:26"
+START=1
+END=10
+count=0
+
+#Script to run automated sql queries 
+#Here i is number of Lessons
+for (( c=$START; c<=$END; c++ ))
+do	
+	#Prepare sql query
+	userId=$(( $RANDOM % 3 + 1 )) #here database has three userid and the dummy data assigned to this user randomly 
+	cDate=$(date '+%Y-%m-%d %H:%M:%S')
+	UUID=$(uuidgen)
+	stop_flg=0
+	delete_flg=0
+  #Date increments by One day
+	startDateTime=$(date '+%Y-%m-%d %H:%M:%S' -d "$startDateTime next day"); 
+	endDateTime=$(date '+%Y-%m-%d %H:%M:%S' -d "$endDateTime next day"); 
+	note="Lorem Ipsum is simply dummy text of the printing and typesetting for user ${i}"
+	approval_flg=0
+	delete_flg=0
+	((count++)) 
+	
+	#mysql DB connection 
+	./mysql.config <<EOF
+	
+	#sql query
+	INSERT INTO t_lesson VALUES($userId, "${i}", "${cDate}", "${UUID}", "${UUID}", $stop_flg, $delete_flg, 
+		  "${startDateTime}", "${endDateTime}", "${note}", $approval_flg, $cancel_flg)	       
+EOF
+done
+echo "${count} Dummy Data Created"
+echo "End of script"
